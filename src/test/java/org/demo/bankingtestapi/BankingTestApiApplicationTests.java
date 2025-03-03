39ae0fec29bc57b4cc83d5864ddbdf25b1235c2c
milindp25 <milindp25@gmail.com>
2025-03-02 19:35:37 -0600

    Initial commit

---
+++ src/test/java/org/demo/bankingtestapi/BankingTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.bankingtestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
